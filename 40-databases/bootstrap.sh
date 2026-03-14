8d746f729a8a502dcfb30fc28243f6312d8e712c
suma <suma.vadla18@gmail.com>
2026-03-14 20:18:51 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 component=$1
 dnf install ansible -y
+
 cd /home/ec2-user
-git clone https://github.com/suma-vadla18/ansible-roboshop-roles-tf.git
-cd anisible-roboshop-roles-tf
+git clone https://github.com/daws-88s/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook  -e component=$component roboshop.yaml
+ansible-playbook -e component=$component roboshop.yaml
