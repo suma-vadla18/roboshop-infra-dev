cf1be9229ee2fb60e60d86197ee3f88998899504
suma <suma.vadla18@gmail.com>
2026-03-15 01:18:53 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -6,13 +6,6 @@ locals {
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
     rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
-    mysql_role_name = join("-", [
-            for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
-        ])
-    mysql_policy_name = join("", [
-            for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
-        ])
-
     common_tags = {
 
         project = var.project
