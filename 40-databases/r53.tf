f4ceebe5893dcf5790e28c83091166556e5a3e40
suma <suma.vadla18@gmail.com>
2026-03-14 18:47:03 +0530

    terraform

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -16,23 +16,23 @@ resource "aws_route53_record" "redis"{
     allow_overwrite = true
 }
 
-resource "aws_route53_record" "mysql"{
-    zone_id = var.zone_id
-    name = "mysql-${var.environment}.${var.domain_name}"
-    type = "A"
-    ttl = "1"
-    records = [aws_instance.mysql.private_ip]
-    allow_overwrite = true
-}
+# resource "aws_route53_record" "mysql"{
+#     zone_id = var.zone_id
+#     name = "mysql-${var.environment}.${var.domain_name}"
+#     type = "A"
+#     ttl = "1"
+#     records = [aws_instance.mysql.private_ip]
+#     allow_overwrite = true
+# }
 
-resource "aws_route53_record" "rabbitmq"{
-    zone_id = var.zone_id
-    name = "rabbitmq-${var.environment}.${var.domain_name}"
-    type = "A"
-    ttl = "1"
-    records = [aws_instance.rabbitmq.private_ip]
-    allow_overwrite = true
-}
+# resource "aws_route53_record" "rabbitmq"{
+#     zone_id = var.zone_id
+#     name = "rabbitmq-${var.environment}.${var.domain_name}"
+#     type = "A"
+#     ttl = "1"
+#     records = [aws_instance.rabbitmq.private_ip]
+#     allow_overwrite = true
+# }
 
 
 
