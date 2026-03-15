c3a2d3ccdb68b0a78b7d6b5384e213029964a9d3
suma <suma.vadla18@gmail.com>
2026-03-15 17:53:37 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -74,3 +74,14 @@ resource "aws_security_group_rule" "rabbitmq_bastion" {
   #which SG you are creating this rule
   security_group_id = local.rabbitmq_sg_id
 }
+
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  from_port         =  80
+  to_port           =  80
+  protocol          = "tcp"
+  #where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  #which SG you are creating this rule
+  security_group_id = local.backend_alb_sg_id
+}
