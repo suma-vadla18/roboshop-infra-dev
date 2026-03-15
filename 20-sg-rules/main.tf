13b9a8662643ff7e144101d2df1db117888a2f74
suma <suma.vadla18@gmail.com>
2026-03-15 18:25:03 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -85,3 +85,25 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   #which SG you are creating this rule
   security_group_id = local.backend_alb_sg_id
 }
+
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  from_port         =  8080
+  to_port           =  8080
+  protocol          = "tcp"
+  #where traffic is coming from
+  source_security_group_id = local.backend_alb_sg_id
+  #which SG you are creating this rule
+  security_group_id = local.catalogue_sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  #where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  #which SG you are creating this rule
+  security_group_id = local.catalogue_sg_id
+}
